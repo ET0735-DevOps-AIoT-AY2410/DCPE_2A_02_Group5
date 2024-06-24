bb19fd336ae7fcf42b0b44f6cc7197b803c8d7cb
Xin Jun <cxj69.23@ichat.sp.edu.sg>
2024-06-24 16:30:40 +0800

    a

---
+++ src/data/users.js
@@ -0,0 +1,23 @@
+// login_details.js
+const users = [
+    {
+        username: 'john',
+        password: 'password',
+        profile: {
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+        },
+        loans: ['Book 1', 'Book 2'],
+        reservations: ['Book 3', 'Book 4']
+    },
+    {
+        username: 'jane',
+        password: 'password',
+        profile: {
+            name: 'Jane Doe',
+            email: 'jane.doe@example.com',
+        },
+        loans: ['Book 5', 'Book 6'],
+        reservations: ['Book 7', 'Book 8']
+    },
+];
