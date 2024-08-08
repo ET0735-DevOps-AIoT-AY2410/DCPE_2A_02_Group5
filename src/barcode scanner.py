9272b692160e2b00c6bfa46be16ef9cd7b4fc259
jaslynyeo <jaslynykl.23@ichat.sp.edu.sg>
2024-08-08 12:13:23 +0800

    fixed camera

--- src/barcode scanner.py
+++ src/barcode scanner.py
@@ -1,26 +1,36 @@
-import time
-from picamera2 import Picamera2
-from pyzbar.pyzbar import decode
+
+from picamera import PiCamera
+from time import sleep
 from PIL import Image
+from pyzbar.pyzbar import decode
+import os
 
 def capture_image(file_path):
-    picam2 = Picamera2()
-    config = picam2.create_still_configuration()
-    picam2.configure(config)
-    picam2.start()
-    time.sleep(2)  # Give the camera some time to adjust to lighting
-    picam2.capture_file(file_path)
-    picam2.stop()
+    camera = PiCamera()
+    camera.resolution = (1440, 1080)
+    camera.start_preview()
+    sleep(2)  # Give the camera some time to adjust to lighting
+    camera.capture(file_path)
+    camera.stop_preview()
+    camera.close()
 
-def scan_barcode(image_path):
-    image = Image.open(image_path)
+def read_barcode(file_path):
+    image = Image.open(file_path)
     barcodes = decode(image)
+    if len(barcodes) == 0:
+        print('No barcode found')
+        return '0000000000'
     for barcode in barcodes:
         barcode_data = barcode.data.decode('utf-8')
         barcode_type = barcode.type
-        print(f"Found {barcode_type} barcode: {barcode_data}")
+        print(f'Found {barcode_type} barcode: {barcode_data}')
+    return barcode_data
+
+def main():
+    image_path = os.path.join(os.path.dirname(__file__), 'barcode.jpg')
+    capture_image(image_path)
+    read_barcode(image_path)
+
 
 if __name__ == "__main__":
-    image_path = 'barcode.jpg'
-    capture_image(image_path)
-    scan_barcode(image_path)
+    main()
