32426824a7bfcd5fc8fb73071763a669747541c1
Xin Jun <CXJ69.23@ichat.sp.edu.sg>
2024-08-14 02:38:03 +0800

    fixing csv issues

--- src/test.py
+++ src/test.py
@@ -1,10 +1,34 @@
 import main
 import csv
 import os
+import datetime
 def main():
-   global location
-   location = "location1"
-   update_status('2302931')
+    global location
+    location = "location1"
+    update_extension('1234567', datetime.datetime.now())
+
+def update_extension(account_id, new_due_date):
+    global location
+    global reserveList
+
+    current_dir = os.getcwd()  # This will get the current working directory
+    file_path = os.path.join(current_dir, 'website', 'reserveList.csv')
+
+    with open(file_path, mode='r', newline='') as file:
+        reader = csv.DictReader(file)
+        reserveList = list(reader)
+
+    new_due_date_str = new_due_date.strftime("%Y-%m-%d %H:%M:%S")
+
+    for item in reserveList:
+        if item['id'] == account_id and item['location'] == location:
+            item['date'] = new_due_date_str
+
+    with open(file_path, mode='w', newline='') as file:
+        fieldnames = ['id', 'bookId', 'location', 'date']  
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(reserveList)
 
 def update_status(account_id):
     global location
